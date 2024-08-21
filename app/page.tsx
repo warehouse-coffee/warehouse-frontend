b03d3db607e020837f6030eb3d60d701df5f4754
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-08-21 11:58:23 +0700

    (REMOVE) Testing Aceternity UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { CardHoverEffectDemo } from '@/components/CardHoverEffectDemo'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CardHoverEffectDemo />
+
     </main>
   )
 }
