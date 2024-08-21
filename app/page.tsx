c5e85e583ac6cc9f4d38e90ccadc6a030a25aeda
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-08-21 14:55:23 +0700

    (UPDATE) Merge branch #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { BreadcrumbDemo } from '@/components/BreadCrumbDemo'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <BreadcrumbDemo />
+
     </main>
   )
 }
