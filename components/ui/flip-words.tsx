b71e0625dfdf2ce97e9926033306b86f23621a48
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-08-28 10:58:38 +0700

    (UPDATE) Home Page

--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -59,7 +59,7 @@ export const FlipWords = ({
           position: 'absolute'
         }}
         className={cn(
-          'z-10 inline-block relative text-left text-neutral-900 dark:text-[#62c5ff] font-bold px-2',
+          'z-10 inline-block relative text-left text-neutral-900 dark:text-[#62c5ff] font-bold px-1',
           className
         )}
         key={currentWord}
