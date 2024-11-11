59afb18f477c1e6efa09ecebdbc6790efe3f0712
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-11-11 10:50:16 +0700

    Update page-loading.tsx

--- components/page-loading.tsx
+++ components/page-loading.tsx
@@ -8,7 +8,7 @@ export default function PageLoading({ loadingText = 'Loading...' }: PageLoadingP
   return (
     <div className="absolute top-0 left-0 flex flex-col gap-3.5 w-full h-screen items-center justify-center bg-black/60 backdrop-blur-sm z-50">
       <Loader color="#62c5ff" size="3.5rem" />
-      <span className="text-[1.2rem] font-medium">{loadingText}</span>
+      <span className="text-[1rem] font-medium">{loadingText}</span>
     </div>
   )
 }
