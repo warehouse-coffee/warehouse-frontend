a221dcc974c3865da309f76c30751f77bf036770
Cevan <110558291+Cevan23@users.noreply.github.com>
2024-12-11 21:53:50 +0700

    add report

--- hooks/report/useReportStorage.ts
+++ hooks/report/useReportStorage.ts
@@ -4,7 +4,7 @@ import { ReportVM } from '@/app/api/web-api-client'
 import { API_ENDPOINTS } from '@/constants'
 
 const fetchReportStorages = async (dateStart: Date, dateEnd: Date): Promise<ReportVM> => {
-  const response = await fetch(`${API_ENDPOINTS.GET_REPORT_STORAGE}?dateStart=${dateStart}&dateEnd=${dateEnd}`)
+  const response = await fetch(`${API_ENDPOINTS.GET_REPORT_STORAGE}?dateStart=${dateStart.toISOString()}&dateEnd=${dateEnd.toISOString()}`)
   if (!response.ok) {
     throw new Error('Failed to fetch report storages')
   }
@@ -13,7 +13,9 @@ const fetchReportStorages = async (dateStart: Date, dateEnd: Date): Promise<Repo
 
 export const useReportStorage = (dateStart: Date, dateEnd: Date) => {
   return useSuspenseQuery<ReportVM>({
-    queryKey: ['reportStorages', dateStart, dateEnd],
-    queryFn: () => fetchReportStorages(dateStart, dateEnd)
+    queryKey: ['reportStorages', dateStart.toISOString(), dateEnd.toISOString()],
+    queryFn: () => fetchReportStorages(dateStart, dateEnd),
+    staleTime: 0,
+    refetchOnWindowFocus: false
   })
 }
