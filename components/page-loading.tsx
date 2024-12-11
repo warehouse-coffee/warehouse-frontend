7027c62f32e56443a0c30985ee0949d8ab0e6d17
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-12-11 22:17:02 +0700

    Update page-loading.tsx

--- components/page-loading.tsx
+++ components/page-loading.tsx
@@ -7,8 +7,8 @@ interface PageLoadingProps {
 export default function PageLoading({ loadingText = 'Loading...' }: PageLoadingProps) {
   return (
     <div className="absolute top-0 left-0 flex flex-col gap-3.5 w-full h-screen items-center justify-center bg-black/60 backdrop-blur-sm z-[99999]">
-      <Loader size="3.5rem" />
-      <span className="text-[1rem] font-medium">{loadingText}</span>
+      <Loader className="stroke-white" size="3.5rem" />
+      <span className="text-[1rem] font-medium text-white dark:text-white">{loadingText}</span>
     </div>
   )
 }
